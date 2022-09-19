9981d65f284bbf77310ac29a6d1b88819347bfce
Rafapp <rpadiper@gmail.com>
2022-09-18 17:36:53 -0700

    added template forreal

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Doot doot la doot doot
         </a>
       </header>
     </div>
