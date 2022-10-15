f54ddb221a267f7c0e47e862441ab6f13e954c4a
Rafapp <rpadiper@gmail.com>
2022-10-15 14:08:51 -0700

    added cover and button

--- my-app/src/Components/Cover.js
+++ my-app/src/Components/Cover.js
@@ -1,13 +1,20 @@
 import Image from 'react-bootstrap/Image'
 import SumoCover from "../Img/SumoCover.gif"
+import { Button } from 'react-bootstrap';
 
 function Cover(){
     return(
-        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: 'auto', width: 'auto'}}>
-            <div style={{backgroundColor:"black", position:'absolute', top:0, left:0, width: "100%", height:"100%"}}>
-            <Image src={SumoCover} style={{width:'100%', opacity:.1}}></Image>
+        <>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: 'auto', width: 'auto'}}>
+                <div style={{overflowX:"hidden",backgroundColor:"black", position:'relative', top:"-20vw", left:0}}>
+                    <Image src={SumoCover} style={{overflowX:"hidden", opacity:.4, width:"100vw"}}></Image>
+                    <h1 style={{position:"absolute", textAlign:"center", top:"50%", left:"50%", transform: "translate(-50%, -50%)", color:"white", fontSize:"7vw"}}>Rafael Padilla Perez</h1>
+                    <h2 style={{position:"absolute", textAlign:"center", top:"70%", left:"50%", transform: "translate(-50%, -50%)", color:"white", fontSize:"3vw"}}>Game Developer, Software Engineer</h2>
+                    <p style={{position:"absolute", textAlign:"center", top:"80%", left:"50%", transform: "translate(-50%, -50%)", color:"white", fontSize:"2vw"}}>Welcome to my porfolio dear traveler!<br></br> You may read more about me below</p>
+                    <Button href='#about' variant="dark" style={{position:"absolute", textAlign:"center", top:"90%", left:"50%", transform: "translate(-50%, -50%)", fontSize:"2vw"}}>About me</Button>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 export default Cover;
