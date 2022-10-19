a0baee40f629a317c6f5b60682f340b47276fdad
Rafapp <rpadiper@gmail.com>
2022-10-19 14:10:00 -0700

    changed image

--- my-app/src/Components/About.js
+++ my-app/src/Components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Image } from "react-bootstrap";
-import RafaIcon from "../Img/RafaIcon.png"
+import RafaPicture from "../Img/RafaPicture.jpg"
 function About(){
     return(
         <div  bg='dark' style={{display:"flex" ,position:"relative", paddingTop:"60%", background:"linear-gradient(to bottom, hsla(0, 0%, 0%, 1) 0%, hsla(0, 0%, 13%, 1) 100%)"}}>
@@ -8,7 +8,7 @@ function About(){
             <p style={{position:"absolute", textAlign:"left", top:"60%", left:"30%", right:"25%", transform: "translate(-50%, -50%)", color:"white", fontSize:"2vw"}}>
             Since I was a kid, I fell in love with computers, technology, and games. <br></br><br></br> There's nothing that brings me more joy than making games and software that brighten other people's days!<br></br><br></br> With all this passion, I'm eager to get my foot on the door as a Game Developer or Software Engineer by bringing my experience and ideas to the table while growing as a person.
             </p>
-            <Image src={RafaIcon} style={{position:"absolute", top:"20%", left:"55%", width:"40vw", height:"40vw"}}></Image>
+            <Image src={RafaPicture} style={{position:"absolute", top:"25%", left:"55%", width:"40vw"}}></Image>
         </div>
     );
 }
